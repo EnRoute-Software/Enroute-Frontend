5038dc47f1af126289b0e5856720f5d12899d726
Andrew Ruegamer <arsailor89@yahoo.com>
2018-10-03 21:14:44 -0400

    initial files

---
+++ enroute/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Enroute</h1>
+          <p>Go sky high without the sky high price.</p>
+        </div>
+      </div>
+    );
+  }
+}
